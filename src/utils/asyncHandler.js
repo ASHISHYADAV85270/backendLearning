1535cb32e838c39fc1e58953c0b7eaa2d685c706
Ashish Yadav <ashish.yadav@aquera.com>
2024-07-12 11:07:19 +0530

    Added util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (fn) => {
+  (req, res, next) => {
+    Promise.resolve(() => {
+      fn(req, res, next);
+    }).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+return { asyncHandler };
+
+// by using try catch
+/*
+const asyncHandler = (fn) => {
+  async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      res.status(err.code || 400).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+};
+
+return { asyncHandler };
+
+*/
