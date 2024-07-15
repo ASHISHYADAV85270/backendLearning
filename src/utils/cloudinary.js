7bf2a3b099a3ee31515b9bd69ae83b15b15fd6b6
Ashish Yadav <ashish.yadav@aquera.com>
2024-07-16 01:05:01 +0530

    added config for multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration for cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async function (localFilePath) {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("File is uploaded on cloudinary", response);
+    return response;
+  } catch (error) {
+    // remove the locally save temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
