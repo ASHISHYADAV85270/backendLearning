f7d6cf320daeb9d70b9a5e13d418b897e4164259
Ashish Yadav <ashish.yadav@aquera.com>
2024-07-27 03:46:39 +0530

    added validation for coverImageLocalPath

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,7 +25,10 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // Step 3: Check for images
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath = "";
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath=req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar File is required");
